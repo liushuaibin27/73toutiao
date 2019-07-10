557bae657508a1d0a22d128e1c5fd9fb17eca7db
liushuaibin27 <387888077@qq.com>
2019-07-10 18:55:43 +0800

    加载router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导包
+import ElementUI from 'element-ui'
+// 引入element样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 引入router
+import router from '@/router'
+// 在全局范围注册element=ui
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+// 导入vue-router
+import VueRouter from 'vue-router'
+// 导入vue
+import Vue from 'vue'
+// 在全局范围注册element-ui
+Vue.use(VueRouter)
+// 初始化router对象
+const router = new VueRouter({
+  routes: []
+})
+export default router

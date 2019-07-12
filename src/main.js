6194793d057b8cd566d336547747c7fb4cd7cec0
liushuaibin27 <387888077@qq.com>
2019-07-12 20:14:53 +0800

    axios默认配置

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import 'element-ui/lib/theme-chalk/index.css'
 // 引入router
 import router from '@/router'
 
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+axios.defaults.headers = {
+  Authorization: 'Bearer ' + JSON.parse(window.sessionStorage.getItem('73toutiao')).token
+}
 Vue.prototype.axios = axios
 // 在全局范围注册element=ui
 Vue.use(ElementUI)
